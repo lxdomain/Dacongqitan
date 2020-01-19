bded800fb7ae86533c54507791e4f613d2dc0a79
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2020-01-19 08:35:24 +0800

    UI processed 1

---
+++ Assets/Scripts/ImageListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageListener : MonoBehaviour
+{
+    [SerializeField]
+    private Image figure;
+    [SerializeField]
+    private Sprite tempSprite;
+
+    private void Awake()
+    {
+        figure = GetComponent<Image>();
+        tempSprite = figure.sprite;
+    }
+
+    private void OnMouseEnter()
+    {
+
+        string path = "Images/yts_hover";
+        Sprite sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+        figure.sprite = sprite;
+    }
+
+    private void OnMouseExit()
+    {
+        figure.sprite = tempSprite;
+    }
+}
--- Assets/Scripts/PanelController.cs
+++ Assets/Scripts/PanelController.cs
@@ -7,69 +7,97 @@ using UnityEngine.UI;
 public class PanelController : MonoBehaviour
 {
     [SerializeField]
-    private List<Button> btnList;
+    private List<Button> barBtnList;
+    //[SerializeField]
+    //private List<Button> itemBtnList;
     [SerializeField]
-    private Button btn1;
+    private List<List<Button>> itemsMgrList;
     [SerializeField]
-    private Button btn2;
-    [SerializeField]
-    private Button btn3;
-    [SerializeField]
-    private Button btn4;
-    [SerializeField]
-    private List<GameObject> viewObjects;
+    private List<GameObject> panelList;
+    
+    private GameObject playerContainer;
+
 
     private void Awake()
     {
-        btnList = new List<Button>();
-        viewObjects = new List<GameObject>();
-        Button[] btnGroup = GameObject.Find("Canvas/Background/LeftCenterPanel").GetComponentsInChildren<Button>();
-        for (int i = 0; i < btnGroup.Length; i++)
-        {
-            btnList.Add(btnGroup[i]);
-        }
-        Debug.Log(btnGroup.Length);
+        Button[] barGroup = GameObject.Find("Canvas/Background/LeftCenterPanel").GetComponentsInChildren<Button>();
+        barBtnList = new List<Button>(barGroup);
+
+        itemsMgrList = new List<List<Button>>();
+
+        panelList = new List<GameObject>();
+
         GameObject[] panelGroup = GameObject.FindGameObjectsWithTag("SubItem");
         for (int i = 0; i < panelGroup.Length; i++)
         {
-            viewObjects.Add(panelGroup[i]);
+            panelList.Add(panelGroup[i]);
             panelGroup[i].SetActive(false);
+
+            Button[] itemGroup = panelGroup[i].GetComponentsInChildren<Button>();
+            List<Button> itemBtnList = new List<Button>(itemGroup);
+            itemsMgrList.Add(itemBtnList);
         }
+
+        playerContainer = GameObject.Find("Canvas/Background/RightCenterPanel/PlayerContainer");
     }
 
     void Start()
     {
-        foreach(Button btn in btnList)
+        foreach(Button btn in barBtnList)
         {
             btn.onClick.AddListener(() => ActivePanel(btn));
         }
-        if (btnList.Count > 0)
+        for(int i = 0; i < itemsMgrList.Count(); i++)
         {
-            viewObjects[0].SetActive(true);
+            // Avoid closure trap.
+            int index = i;
+             foreach(Button item in itemsMgrList[i])
+             {
+                        item.onClick.AddListener(() => toggleInnerText(item, index));
+             }
         }
     }
 
     
     private void ActivePanel(Button btn)
     {
-        for(int i = 0; i < btnList.Count(); i++)
+        playerContainer.SetActive(false);
+        for (int i = 0; i < barBtnList.Count(); i++)
         {
-            bool isSelect = (btnList[i] == btn);
-            btnList[i].interactable = !isSelect;
+            bool isSelect = (barBtnList[i] == btn);
+            barBtnList[i].interactable = !isSelect;
             if (isSelect)
             {
                 // Show the panel related to the current clicked button.
-                viewObjects[i].SetActive(true);
+                panelList[i].SetActive(true);
             }
-            for(int j = 0; j < viewObjects.Count(); j++)
+            for(int j = 0; j < panelList.Count(); j++)
             {
                 // If not related.
                 if (j != i && isSelect)
                 {
                     // Hide the panel that not related to the current clicked button.
-                    viewObjects[j].SetActive(false);
+                    panelList[j].SetActive(false);
                 }
             }
         }
     }
+
+    private void toggleInnerText(Button btn, int index)
+    {
+        Text itemText = btn.transform.Find("Text").GetComponent<Text>();
+        //print(itemText.text.ToString());
+        Text innerText = barBtnList[index].transform.Find("DynamicText").GetComponent<Text>();
+        innerText.text = itemText.text.ToString();
+
+        if(index == 2)
+        {
+            innerText.text = itemText.text.ToString().Substring(0, 4);
+        }
+
+        panelList[index].SetActive(false);
+        playerContainer.SetActive(true);
+    }
+
 }
+    
