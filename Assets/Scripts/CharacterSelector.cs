7040749486364c6aa888b1971b2dc546468842c5
Jocoboy <50202665+Jocoboy@users.noreply.github.com>
2020-01-26 18:10:33 +0800

    UI processed 3, try to using Sprite Swap instead

---
+++ Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelector : MonoBehaviour
+{    
+    //[SerializeField]
+    //private List<Button> modeBtnList;
+    [SerializeField]
+    private List<Button> selectorBtnList;
+    [SerializeField]
+    private List<GameObject> activePanelList;
+    [SerializeField]
+    private List<GameObject> modeSetList;
+    [SerializeField]
+    private List<GameObject> preSetList;
+    private GameObject characterPanel;
+    private Button backBtn;
+    private readonly string abs_path = "Canvas/Background/RightCenterPanel/PlayerContainer";
+    private readonly string[] rel_paths = { "/Player_01", "/Player_02", "/Player_03", "/Player_04" };
+
+    private void Awake()
+    {
+        //modeBtnList = new List<Button>();
+        selectorBtnList = new List<Button>();
+        activePanelList = new List<GameObject>();
+        modeSetList = new List<GameObject>();
+        preSetList = new List<GameObject>();
+        GameObject[] panelGroup = GameObject.FindGameObjectsWithTag("Configuration");
+        foreach (GameObject obj in panelGroup)
+        {
+            activePanelList.Add(obj);
+        }
+        characterPanel = GameObject.Find("Canvas/Background/CenterPanel");
+        characterPanel.SetActive(false);
+        backBtn = GameObject.Find("Canvas/Background/TopMask").GetComponentInChildren<Button>();
+        backBtn.gameObject.SetActive(false);
+        //getBtnsByAbsPath(modeBtnList, "/ModeSet");
+        getBtnsByAbsPath(selectorBtnList, "/Content");
+        //for (int i = 0; i < rel_paths.Length; i++)
+        //{
+        //    Button[] selectorGroup = GameObject.Find(abs_path + rel_paths[i] + "/Content").GetComponentsInChildren<Button>();
+        //    for (int j = 0; j < selectorGroup.Length; j++)
+        //    {
+        //        selectorBtnList.Add(selectorGroup[j]);
+        //    }
+        //}
+        getGameObjectsByAbsPath(modeSetList, "/ModeSet");
+        getGameObjectsByAbsPath(preSetList, "/PreSet");
+        //for(int i = 0; i < rel_paths.Length; i++)
+        //{
+        //    GameObject modeSet = GameObject.Find(abs_path + rel_paths[i] + "/ModeSet");
+        //    modeSetList.Add(modeSet);
+        //}
+    }
+
+    private void Start()
+    {
+        // Binding events for a group of mode set buttons.
+        for (int i = 0; i < modeSetList.Count(); i++)
+        {
+            // Avoid closure trap.
+            int index = i;
+            foreach (Button btn in modeSetList[i].GetComponentsInChildren<Button>())
+            {
+                btn.onClick.AddListener(() =>
+                {
+                    Text mode = modeSetList[index].GetComponentsInChildren<Text>()[1];
+                    mode.text = (mode.text == "手动" ? "电脑" : "手动");
+                });
+            }
+        }
+
+        // Binding events for a group of pre set buttons.
+        for(int i = 0; i < preSetList.Count(); i++)
+        {
+            Button[] btnPair = preSetList[i].GetComponentsInChildren<Button>();
+            Text cardsNum = preSetList[i].GetComponentsInChildren<Text>()[1];
+            string tag = "预设卡组";
+            int index = cardsNum.text[cardsNum.text.Length - 1] - '1';
+            btnPair[0].onClick.AddListener(() =>
+            {
+                index = (index+3) % 4;
+                cardsNum.text = tag + (index+1);
+            });
+            btnPair[1].onClick.AddListener(() =>
+            {
+                index = (index + 1) % 4;
+                cardsNum.text = tag + (index+1);
+            });
+        }
+
+        // Binding events for a group of selector buttons.
+        foreach (Button btn in selectorBtnList)
+        {
+            btn.onClick.AddListener(() => {
+                DisablePanels(activePanelList);
+                ActiveCharacterPanel(btn);
+            });
+        }
+        // Binding event for the back button.
+        backBtn.onClick.AddListener(() =>
+        {
+            activePanelList[0].transform.Find("Text").GetComponentInChildren<Text>().text = "赛局设置>>>";
+            foreach (GameObject panel in activePanelList)
+            {
+                //  avoid the top mask hidden incorrectly.
+                if (panel.name != "TopMask")
+                {
+                        panel.SetActive(!panel.activeSelf);
+                }
+            }
+            backBtn.gameObject.SetActive(false);
+        });
+    }
+
+    private void getBtnsByAbsPath(List<Button> tar,string rel_path)
+    {
+        for (int i = 0; i < rel_paths.Length; i++)
+        {
+            Button[] selectorGroup = GameObject.Find(abs_path + rel_paths[i] + rel_path).GetComponentsInChildren<Button>();
+            for (int j = 0; j < selectorGroup.Length; j++)
+            {
+                tar.Add(selectorGroup[j]);
+            }
+        }
+    }
+
+    private void getGameObjectsByAbsPath(List<GameObject> tar,string rel_path)
+    {
+        for (int i = 0; i < rel_paths.Length; i++)
+        {
+            GameObject modeSet = GameObject.Find(abs_path + rel_paths[i] + rel_path);
+            tar.Add(modeSet);
+        }
+    }
+
+    private void DisablePanels(List<GameObject> panels)
+    {
+        for(int i = 1; i <panels.Count()-1;i++)
+        {
+            panels[i].SetActive(false);
+        }
+    }
+
+    private void ActiveCharacterPanel(Button btn)
+    {
+        activePanelList[0].transform.Find("Text").GetComponentInChildren<Text>().text = "更换角色>>>";
+        activePanelList[activePanelList.Count()-1].SetActive(true);
+        backBtn.gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/PanelToggle.cs
+++ Assets/Scripts/PanelToggle.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PanelController : MonoBehaviour
+public class PanelToggle : MonoBehaviour
 {
     [SerializeField]
     private List<Button> barBtnList;
